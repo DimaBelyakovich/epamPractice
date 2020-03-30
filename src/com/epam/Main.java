ecb197a4a5e3cd99dab8cadf36b7eea4f05c83a5
DimaBelyakovich <dimabelyakovich.mmf@gmail.com>
2020-03-30 18:59:01 +0300

    config project

---
+++ src/com/epam/Main.java
@@ -0,0 +1,11 @@
+package com.epam;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    static Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        logger.info(12312);
+    }
+}
